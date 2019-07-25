460aecff7cb634299d5c3836991f1fdf57ff1dfc
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-25 00:34:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,11 +4,7 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
+
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do | kids_name, age |
